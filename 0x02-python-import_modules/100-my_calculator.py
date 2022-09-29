5c0b62e6535dccfa0a9405c3b187926bcfc47fef
Joshua Taiwo <98131071+joshiue@users.noreply.github.com>
2022-09-29 02:53:30 +0100

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,37 +1,23 @@
 #!/usr/bin/python3
-from sys import argv
-from calculator_1 import add, sub, mul, div
-
-if __name__ == '__main__':
-    """
-    Handles basic operations
-    Performs basic operations like addition, substraction,
-    multiplication and division between two numbers.
-    The program will execute an operation between two numbers
-    selected by the operator sent to the program.
-    """
-
-    l_av = len(argv) - 1
-    
-    if l_av == 3:
-        operator = argv[2]
-        num_a = int(argv[1])
-        num_b = int(argv[3])
-        if operator == '+':
-            res = add(num_a, num_b)
-            print('{:d} + {:d} = {:d}'.format(num_a, num_b, res))
-        elif operator == '-':
-            res = sub(num_a, num_b)
-            print('{:d} - {:d} = {:d}'.format(num_a, num_b, res))
-        elif operator == '*':
-            res = mul(num_a, num_b)
-            print('{:d} * {:d} = {:d}'.format(num_a, num_b, res))
-        elif operator == '/':
-            res = div(num_a, num_b)
-            print('{:d} / {:d} = {:d}'.format(num_a, num_b, res))
-        else:
-            print('Unknown operator. Available operators: +, -, * and /')
-            exit(1)
-    else:
-        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
+if __name__ == "__main__":
+    from sys import argv
+    from calculator_1 import add, sub, mul, div
+    num_arg = len(argv[1:])
+    if num_arg != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
+    sign = ['+', '-', '*', '/']
+    if argv[2] not in sign:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    num1 = int(argv[1])
+    num2 = int(argv[3])
+    op = argv[2]
+    if op == "+":
+        print("{:d} + {:d} = {:d}".format(num1, num2, add(num1, num2)))
+    if op == "-":
+        print("{:d} - {:d} = {:d}".format(num1, num2, sub(num1, num2)))
+    if op == "*":
+        print("{:d} * {:d} = {:d}".format(num1, num2, mul(num1, num2)))
+    if op == "/":
+        print("{:d} / {:d} = {:d}".format(num1, num2, div(num1, num2)))
