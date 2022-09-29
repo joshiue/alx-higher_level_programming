33537bd780d7c19e49ff1f0fb5447a36f47c7a11
Joshua Taiwo <98131071+joshiue@users.noreply.github.com>
2022-09-29 02:55:23 +0100

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,23 +1,28 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from sys import argv
-    from calculator_1 import add, sub, mul, div
-    num_arg = len(argv[1:])
-    if num_arg != 3:
+from calculator_1 import add, sub, mul, div
+
+
+def arg_calc(argv):
+    n = len(argv) - 1
+    if n != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    sign = ['+', '-', '*', '/']
-    if argv[2] not in sign:
+    a = int(argv[1])
+    op = argv[2]
+    b = int(argv[3])
+    if op == '+':
+        print("{:d} {:s} {:d} = {:d}".format(a, op, b, add(a, b)))
+    elif op == '-':
+        print("{:d} {:s} {:d} = {:d}".format(a, op, b, sub(a, b)))
+    elif op == '*':
+        print("{:d} {:s} {:d} = {:d}".format(a, op, b, mul(a, b)))
+    elif op == '/':
+        print("{:d} {:s} {:d} = {:d}".format(a, op, b, div(a, b)))
+    else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
-    num1 = int(argv[1])
-    num2 = int(argv[3])
-    op = argv[2]
-    if op == "+":
-        print("{:d} + {:d} = {:d}".format(num1, num2, add(num1, num2)))
-    if op == "-":
-        print("{:d} - {:d} = {:d}".format(num1, num2, sub(num1, num2)))
-    if op == "*":
-        print("{:d} * {:d} = {:d}".format(num1, num2, mul(num1, num2)))
-    if op == "/":
-        print("{:d} / {:d} = {:d}".format(num1, num2, div(num1, num2)))
+
+
+if __name__ == "__main__":
+    import sys
+    arg_calc(sys.argv)
